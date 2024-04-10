a3fed107ac8405a9293aac809d121a7412d37080
Mathias Schembri <mathiasschembri@gmail.com>
2024-04-10 17:19:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import Main from './components/Main'
-const {ipcRenderer} = window.require('electron')
+import Main from './components/Main';
+import EyeTrackingComponent from './components/EyeTrackingComponent'; 
+
+const { ipcRenderer } = window.require('electron');
 
 class App extends Component {
 
-componentDidMount(){
-  // notify the main process that the React application was loaded successfully
-  ipcRenderer.send('windowLoaded');    
-}
+  componentDidMount() {
+    // notify the main process that the React application was loaded successfully
+    ipcRenderer.send('windowLoaded');    
+  }
 
   render() {
     return (
       <div>
-        <Main/>
+        <Main />
+        <EyeTrackingComponent /> 
       </div>
     );
   }
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
